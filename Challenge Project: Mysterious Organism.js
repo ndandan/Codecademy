b2bcabb64f7dffa6b506805257dcaeb947ce732b
Dan Morgan <ndandan1369@yahoo.com>
2023-01-18 19:22:28 -0600

    Update Challenge Project: Mysterious Organism.js

--- Challenge Project: Mysterious Organism.js
+++ Challenge Project: Mysterious Organism.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -12,52 +12,51 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
-
- const pAequorFactory = (num, arr) => {
+//Creates a sample from the factory
+const pAequorFactory = (num, arr) => {
   return {
     specimenNum: num,
     dna: arr,
-    mutate(){
-      let ranPick = dna[Math.floor(Math.random()*14)]
+    // mutates a random base dna
+    mutate() {
+      let ranPick = dna[Math.floor(Math.random() * 14)];
       let index = dna.indexOf(ranPick);
       let mBase = dna[index];
       let newBase = returnRandBase();
-      while(newBase === mBase){
+      while (newBase === mBase) {
         newBase = returnRandBase();
       }
-      return this.dna[index] = newBase;
+      return (this.dna[index] = newBase);
     },
-    compareDNA(pAequor){
-      for(let x = 0; x < this.dna.length; x++){
-        if(this.dna[x]===pAequor.dna[x]){
+    //compares this sample against another to find the % a like
+    compareDNA(pAequor) {
+      for (let x = 0; x < this.dna.length; x++) {
+        if (this.dna[x] === pAequor.dna[x]) {
           counter++;
         }
       }
-      return `specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${(counter / 15)*100}% DNA in common`
+      return `specimen #${this.specimenNum} and specimen #${
+        pAequor.specimenNum
+      } have ${(counter / 15) * 100}% DNA in common`;
     },
-    willLikelySurvive(){
-      let matched = this.dna.filter((z) => z === 'C' || z === 'G');
-      console.log(matched.length);
-      return (matched.length/this.dna.length) >=0.60 ? true : false;
-      },
-      complementStrand(){
-        
-      }
+    // checks the dna to see if 60% or more is made of C or G
+    willLikelySurvive() {
+      let matched = this.dna.filter((z) => z === "C" || z === "G");
+      return (this.likelySurvive =
+        matched.length / this.dna.length >= 0.6 ? true : false);
     }
   };
+};
 
-// const test1 = pAequorFactory(Math.floor(Math.random()*25), mockUpStrand());
-// const test2 = pAequorFactory(Math.floor(Math.random()*25), mockUpStrand());
-
-// console.log(test1.willLikelySurvive());
 
 let samples = [];
-
+//Function t ocreate 30 samples with the factory 
 const cSamples = () => {
-  for(let d = 0; d < 30; d++){
-    samples.push(pAequorFactory((d+1), mockUpStrand()));
+  for (let d = 0; d < 30; d++) {
+    samples.push(pAequorFactory(d + 1, mockUpStrand()));
+    samples[d].willLikelySurvive();
   }
-}
+};
 
 cSamples();
 
